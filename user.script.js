a70ef2d999721c0dd28118d4175d32b34ad52eb9
Nisala Kalupahana <avishkank@gmail.com>
2019-08-30 13:37:46 -0700

    Create user.script.js

---
+++ user.script.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Illinois Save
+// @namespace    https://courseware.illinois.edu
+// @version      1.0
+// @description  Enable save shortcut on NetMath
+// @author       Nisala Kalupahana
+// @match        https://courseware.illinois.edu
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.onkeydown = function (e) {
+        if ((e.metaKey || e.ctrlKey) && e.keyCode == 83) {
+            console.log(unsafeWindow.document);
+            unsafeWindow.frames["uxiframe-1082-frame"].document.getElementById("ext-gen30").click();
+            e.preventDefault();
+            return false;
+        }
+    };
+})();
